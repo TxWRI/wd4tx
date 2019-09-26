017486f221dffe9c56e664eab31730d7f82f0203
mps9506 <mpschramm@gmail.com>
2019-09-26 15:49:45 -0500

    coastal hydrology and geometry functions

---
+++ R/download_coastal_geometry.R
@@ -0,0 +1,19 @@
+#' Download 'TWDB' Coastal Geographic Boundaries
+#'
+#' @param type character, required. One of \code{c("basin", "bay", "estuary",
+#'   "sub_watershed", "watershed")}
+#'
+#' @return sf
+#' @export
+#'
+download_coastal_geometry <- function(type) {
+  url <- sprintf("https://waterdatafortexas.org/coastal/api/geometries/%s",
+                 type)
+
+  ## extra data in the geojson, need to figure out how to get the data
+  ## into the sf
+
+  content <- get_well_meta(url,
+                           path = NULL)
+  return(content)
+}
---
+++ R/download_coastal_hydrology.R
@@ -0,0 +1,29 @@
+
+#' Download 'TWDB' Historical Bay and Estuary Freshwater Inflow Data
+#'
+#' @param geoid character, required.
+#' @param resample character, optional. By default, data is returned in daily
+#'   interval. Specify one of \code{c("month", "year")} to get monthly or yearly
+#'   summed values.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+download_coastal_hydrology <- function(geoid, resample = NULL) {
+
+  url <- sprintf("https://waterdatafortexas.org/coastal/api/hydrology/%s/timeseries",
+                 geoid)
+  path = NULL
+  if(is.null(resample)) args = list()
+  else args = list(resample = resample)
+
+  ## download
+  content <- get_coastal_sites(url, path, args = args)
+
+  ## parse the returned json
+  content <- jsonlite::fromJSON(content)
+
+  ## return as tibble (need to specify column types)
+  content <- tibble::as_tibble(content)
+}
